014c7dd80635d7bcc0538ef9063bfdb427a7c567
박찬규 <126973231+pck-llight@users.noreply.github.com>
2025-04-12 21:22:10 +0900

    기본 사이트

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,98 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import styled from "styled-components";
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App(){
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <Screen>
+            <MainText>
+                저는 즉흥적으로 행동해<br/>끝을 보는 개발자
+            </MainText>
+            {/*<MainText>*/}
+            {/*    <PrimaryText>박찬규</PrimaryText>입니다.*/}
+            {/*</MainText>*/}
+            <CodeChan>Code.Chan</CodeChan>
+            <LinkBar>
+                <Link href={"https://www.instagram.com/chankyu_07/"}>instagram</Link>
+                <Link href={"https://github.com/pck-llight"}>github</Link>
+            </LinkBar>
+            <ContactBar>
+                <ContactText>contact</ContactText>
+                <ContactText>to@codechan.dev</ContactText>
+            </ContactBar>
+        </Screen>
+    )
 }
 
-export default App
+const Screen = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100dvh;
+    gap: 20px;
+`
+const MainText = styled.span`
+    color: #EBEBEB;
+    text-align: center;
+    font-family: "UhBee MiMi";
+    font-size: 53px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    letter-spacing: -1.59px;
+`
+const PrimaryText = styled(MainText)`
+    color: #AA72FF;
+    text-align: center;
+    font-family: "UhBee MiMi";
+    font-size: 53px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    letter-spacing: -1.59px;
+`
+
+const CodeChan =styled.span`
+    color: #EBEBEB;
+    font-family: SUIT;
+    font-size: 48px;
+    font-style: normal;
+    font-weight: 200;
+    line-height: normal;
+`
+const LinkBar = styled.div`
+    display: flex;
+    align-items: center;
+    gap: 25px;
+`
+
+const Link = styled.a`
+    color: #AA72FF;
+    text-align: center;
+    font-family: "UhBee DongKyung";
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    letter-spacing: -0.42px;
+    text-decoration: none;
+`
+const ContactBar = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: row;
+    gap: 16px;
+`
+
+const ContactText = styled.span`
+    color: #FFF;
+    text-align: center;
+    font-family: SUIT;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 300;
+    line-height: normal;
+    letter-spacing: -0.48px;
+`
